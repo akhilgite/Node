da01f6a6194cff84779f8e751f53c8380e9d528d
Nikhil Sakhare <nikhil.sakhare@viacomcontractor.com>
2019-08-26 18:56:36 +0530

    index file changed for calling firebase queries on button click

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const firebase = require('firebase');
-//const functions = require('firebase-functions');
-//const firebase =  require("firebase-admin");
-//const express = require("express");
-//const engines = require("consolidate");
+const functions = require('firebase-functions');
+const firebaseAdmin =  require("firebase-admin");
+const express = require("express");
+const engines = require("consolidate");
 
 /*const firebaseApp = firebase.initializeApp(
     functions.config().firebase
@@ -12,18 +12,15 @@ const firebase = require('firebase');
     const ref = firebaseApp.database().ref("facts");
     return ref.once("value").then(snap=> snap.val());
 };
-
+*/
 const app = express();
 app.engine("hbs", engines.handlebars);
 app.set("views", "./views");
 app.set("view engine", "hbs");
 
 app.get("/",(request, response)=>{
-    response.set("Cache-control","public, max-age=300, s-maxage=600" );
-    getFacts().then(facts=>{
-        response.render("index",{ facts });
-    });
-});*/
+    response.send("Hello from root");
+});
 
 
 
@@ -32,17 +29,36 @@ firebase.initializeApp({
     databaseURL: "https://mayur-mogre.firebaseio.com/"
 });
 
-var ref = firebase.database().ref("facts");
-var messagesRef = ref.child("messages");
-messagesRef.push({
+var ref = firebase.database().ref
+
+var messageRef = messagesRef.push();
+var messageKey = messageRef.key;
+var payload = {};
+
+var message = {
+    text: "hey guys"
+};
+
+payload['userMessages/'+ messageKey] = message;
+payload['logs/'+ messageKey] = message;
+
+ref.update(payload);
+
+ref.once('value').then(function(snap) {
+    console.log(snap.key, "\n\n");
+    console.log(snap.ref.toString(), "\n\n");   
+    console.log(snap.val(), "\n\n");   
+});
+
+/*messagesRef.push({
     name: "Akhil",
     admin: true,
     count: 1,
     text: "Hey"
-});
+});*/
 
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-//exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(app);
